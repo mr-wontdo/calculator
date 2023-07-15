300df6c0e5411a63ac9570b488a9f1de06a2b80c
mr-wontdo <122188740+mr-wontdo@users.noreply.github.com>
2023-07-15 15:21:27 -0500

    Add event listeners to number, add, and equal buttons

--- javascript.js
+++ javascript.js
@@ -7,13 +7,42 @@ const display = document.querySelector('.display');
 const numberButtons = document.querySelectorAll('.number');
 numberButtons.forEach(button => {
     button.addEventListener('click', (e) => {
+        if (clearDOM === true) {
+            display.textContent = '';
+            clearDOM = false;
+        }
+        if (display.textContent === '0') {
+            display.textContent = '';
+        }
         display.textContent += e.target.firstChild.textContent;
     });
 });
 
 const addButton = document.querySelector('.add');
 addButton.addEventListener('click', () => {
-    z = add;
+    if (x === null && y === null) {
+        x = +display.textContent;
+        z = add;
+        clearDOM = true;
+    } else if (x !== null && y === null) {
+        y = +display.textContent;
+        z = add;
+        x = z(x, y);
+        y = null;
+        z = null;
+        display.textContent = x;
+        clearDOM = true;
+    }
+});
+
+const equalButton = document.querySelector('.equal');
+equalButton.addEventListener('click', () => {
+    y = +display.textContent;
+    x = z(x, y)
+    display.textContent = x;
+    y = null;
+    z = null;
+    clearDOM = true;
 });
 
 function equal(x, y, z) {
